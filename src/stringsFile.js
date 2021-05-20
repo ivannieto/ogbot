95bb4c0f29fb8a92f028fdad6e22208e67a1847b
Ivan Nieto Sousa <ivan.nieto.sousa@gmail.com>
2021-05-20 16:14:12 +0200

    Cleanup stringsFile

--- src/stringsFile.js
+++ src/stringsFile.js
@@ -11,14 +11,3 @@ Available commands:
 
 This bot makes use of Coingecko API to retrieve data and Cryptowat.ch API to retrieve OHLC data.`
 
-module.exports.HELP_CREATE_DRAW = `Ok, let's start
-1. We create an address for you if you don't have it yet
-2. You need to fund your address in order to create a payable draw
-3.`
-
-// questions
-module.exports.QUESTION_DRAW_NAME = `What's the name for the draw?`
-module.exports.QUESTION_DRAW_END_DATE = `When will the draw end? Write it in the format *YYYY/MM/DDTH:m:s* - e.g.: 2021/05/21T22:00:00`
-module.exports.QUESTION_YES_OR_NO = 'Are you ok with this? Y or N'
-module.exports.QUESTION_POLL_NAME = `Write the question for the poll`
-module.exports.QUESTION_GUESS_COIN = `Which coin do you want to use?`
