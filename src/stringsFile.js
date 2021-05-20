7b786a2bd43d9b0a36da4ab160e4504c8e1517b8
Ivan Nieto Sousa <ivan.nieto.sousa@gmail.com>
2021-05-20 16:13:17 +0200

    Update Help

--- src/stringsFile.js
+++ src/stringsFile.js
@@ -4,7 +4,7 @@ The crypto companion for real OG's
 Available commands:
 /help - Displays this help
 /pr - <coin> Asks the price for a coin
-/chart - <coin> <vs_coin> <timeframe (1m,5m,15m,1h,4h,1d...)> <exchange> Displays a chart in days timeframes 
+/chart - <coin> <vs_coin> <timeframe (1m,5m,15m,1h,4h,1d...)> <exchange> Displays a chart which bars are specified by user
 /status - How is doing the market right now?
 /df - Displays DeFi data
 /trending - Displays trending coins data
